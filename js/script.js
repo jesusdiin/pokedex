5cfcd1e70eeda5c90970e91d0fcde7c5cbca38c6
Jesús Hernández <cjesus02hdz@gmail.com>
2022-07-28 16:54:44 -0500

    add ui

--- js/script.js
+++ js/script.js
@@ -1,31 +1,61 @@
 const cardsDiv = document.getElementById('cardsDiv')
 
+
+
+
+
+
+
 const getPokemon = async () => {
 	let url = "https://pokeapi.co/api/v2/pokemon/";
-	let elemento = document.getElementById('cardsDiv');
+	let group1 = document.getElementById('group1');
+	let group2 = document.getElementById('group2')
 
 	const data = await fetch(url);
 	let pokes = await data.json();
 
 	// console.log(pokes)
 
+	class Pokemon {
+		constructor(name, url) {
+			this.name = name;
+			this.url = url;
+		}
+		obtener
+	}
+
 	for (let index = 0; index < pokes.results.length; index++) {
 		const pokemon = pokes.results[index];
 		console.log(pokemon)
 		var cardComponent = `
-		<div class='col mb-4'>
-			<div class='card'>
-				<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png" alt="">
+		<div class='col-sm-2 col-md-3 col-lg-3 row' id='cardId'>
+			<div class='card' id="">
+				<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index}.svg" alt="" class="avatarImg">
 				<div class="card-body">
 				<h5 class='card-title'>${pokemon.name}</h5>
 				</div>
 			</div>
 		</div>
 		`
-
-		elemento.innerHTML += cardComponent;
-		
+		group1.innerHTML += cardComponent;
+		group2.innerHTML += cardComponent;
 	}
+
+	// for (let index = 0; index < pokes.results.length; index++) {
+	// 	const pokemon = pokes.results[index];
+	// 	console.log(pokemon)
+	// 	var cardComponent = `
+	// 	<div class='col-sm-2 col-md-3 col-lg-3 row' id='cardId'>
+	// 		<div class='card' id="">
+	// 			<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index}.svg" alt="" class="avatarImg">
+	// 			<div class="card-body">
+	// 			<h5 class='card-title'>${pokemon.name}</h5>
+	// 			</div>
+	// 		</div>
+	// 	</div>
+	// 	`
+	// 	group2.innerHTML += cardComponent;
+	// }
 
 
 }
